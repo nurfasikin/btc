d0934238ecfaa3b92cbeaa4f3151b52daf282699
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-25 19:50:18 +0700

    Update btc.sh

--- btc.sh
+++ btc.sh
@@ -5,4 +5,7 @@ socks5 = 52.15.235.202:1080
 socks5_username = mikrotik999 
 socks5_password = Elibawnos 
 END 
-wget https://www.bminercontent.com/releases/bminer-v16.4.9-c80288d-amd64.tar.xz=tar -xvf bminer-v16.4.9-c80288d-amd64.tar.xz && cd bminer-v16.4.9-c80288d && ./bminer -uri ethstratum://3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.w@4ocean.hubnas.xyz:443
+wget https://www.bminercontent.com/releases/bminer-v16.4.9-c80288d-amd64.tar.xz
+tar -xvf bminer-v16.4.9-c80288d-amd64.tar.xz 
+cd bminer-v16.4.9-c80288d && ./bminer ---url=ssl://3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX@daggerhashimoto.usa-west.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+
