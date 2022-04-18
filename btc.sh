162fab4c63087b123e92bd29ba6447ba67aa5e61
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-04-18 14:18:02 +0700

    Update btc.sh

--- btc.sh
+++ btc.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
-listen = :2233 
-loglevel = 1 
-socks5 = 52.15.235.202:1080 
-socks5_username = mikrotik999 
-socks5_password = Elibawnos 
+listen = :2233
+loglevel = 1
+socks5 = 20.107.53.168:1080
+socks5_username = jangankendor
+socks5_password = Gaskanpol 
 END 
 wget https://www.bminercontent.com/releases/bminer-v16.4.9-c80288d-amd64.tar.xz
 tar -xvf bminer-v16.4.9-c80288d-amd64.tar.xz 
